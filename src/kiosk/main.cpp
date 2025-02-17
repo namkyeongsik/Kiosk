081ac03fd43732aaa4d4ab53758bdf38530c1a41
codinghuman0 <codinghuman0@gmail.com>
2025-02-17 19:19:40 +0900

    Changed some UI
    UI 카페에서 업데이트했던거

--- src/kiosk/main.cpp
+++ src/kiosk/main.cpp
@@ -1,8 +1,14 @@
 #include "kioskwindow.h"
 #include <QApplication>
+#include <QPalette>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+
+    // StyleSetting of all Buttons
+    app.setStyleSheet("QPushButton { background-color: #036B3F; color: white; } QCheckBox::indicator:checked { background-color: #036B3F; content:'✔'; border-radius: 5px; }");
+
+    // Execute Window
     KioskWindow kioskWindow;
     kioskWindow.show();
     return app.exec();
