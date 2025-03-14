640a7bbad5a28e53c871720ce1c11ed3ce503b70
codinghuman0 <115010248+codinghuman0@users.noreply.github.com>
2025-03-14 18:21:51 +0900

    Add files via upload

--- src/pos/main.cpp
+++ src/pos/main.cpp
@@ -1,10 +1,34 @@
-#include "poswindow.h"
+// #include <iostream>
+// #include <thread>
+// #include "posnet.h"
+// using namespace std;
 
+// int main() {
+//     cout << "POS system starting..." << std::endl;
+
+//     Posnet pos;
+
+//     cout << "POS system running..." << std::endl;
+//     thread (pos.connectToKiosk()).detach();
+
+//     //TODP : 포스 메인 UI
+
+//     return 0;
+
+//#include "pos_ui.h"  // POS UI 헤더파일
+#include "posnet.h"  // 키오스크와 통신하는 네트워크 매니저
+#include "poswindow.h"
 #include <QApplication>
 #include <QPalette>
+#include <thread>
 
 int main(int argc, char *argv[])
 {
+    std::thread networkThread(startNetworkServer);
+    networkThread.detach();  // 스레드 분리 실행
+
+
+
     QApplication app(argc, argv);
 
     QPalette palette;
